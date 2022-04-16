300dc1c053ef2f4a6f0a32f1d52316caec783fe3
Nguyễn Ngọc Ninh <78458373+Ninh236@users.noreply.github.com>
2022-04-16 16:12:41 +0700

    upload demo profile page

---
+++ demo-app/src/page/ProfilePage.vue
@@ -0,0 +1,34 @@
+<template>
+  <tool-bar></tool-bar>
+  <section class="background-image-section">
+    <img class="background-image" src="../assets/logo.png" alt="" />
+  </section>
+</template>
+
+<script>
+import ToolBar from "../components/ToolBar.vue";
+
+export default {
+  component: {
+    'tool-bar': ToolBar,
+  },
+};
+</script>
+
+<style>
+.background-image-section {
+  text-align: left;
+  display: block;
+  float: left;
+  width: 100%;
+  height: 50vh;
+  position: relative;
+}
+
+.background-image {
+  box-sizing: border-box;
+  float: left;
+  width: 100%;
+  height: 100%;
+}
+</style>
